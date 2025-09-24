2740e49ec7ddf76a32ca965168d20909648b0327
Prakash-1619 <poojariprakash88@gmail.com>
2025-09-24 18:17:07 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -1528,7 +1528,7 @@ elif page == "V2.1":
         Burj_Khalifa = "dt_model_Burj_Khalifa.pkl"
         Business_Bay = "dt_model_Business_Bay.pkl"
         Hadaeq_Sheikh_Mohammed_Bin_Rashid = "dt_model_Hadaeq_Sheikh_Mohammed_Bin_Rashid.pkl"
-        Jabel_Ali_First = "dt_model_Jabel_Ali_First.pkl"
+        Jabal_Ali_First = "dt_model_Jabal_Ali_First.pkl"
         Madinat_Al_Mataar = "dt_model_Madinat_Al_Mataar.pkl"
         Madinat_Dubai_Almelaheyah = "dt_model_Madinat_Dubai_Almelaheyah.pkl"
         Marsa_Dubai = "dt_model_Marsa_Dubai.pkl"
@@ -1542,7 +1542,7 @@ elif page == "V2.1":
             Al_Barsha_South_Fifth, Al_Barsha_South_Fourth, Al_Barshaa_South_Third,
             Al_Hebiah_Fourth, Al_Khairan_First, Al_Merkadh, Al_Thanyah_Fifth,
             Al_Warsan_First, Al_Yelayiss_2, Bukadra, Burj_Khalifa, Business_Bay,
-            Hadaeq_Sheikh_Mohammed_Bin_Rashid, Jabel_Ali_First, Madinat_Al_Mataar,
+            Hadaeq_Sheikh_Mohammed_Bin_Rashid, Jabal_Ali_First, Madinat_Al_Mataar,
             Madinat_Dubai_Almelaheyah, Marsa_Dubai, MeAisem_First, Nadd_Hessa, Wadi_Al_Safa_5
         ]
         
