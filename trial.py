9f13c6cbe693e614e0c64a0d467824654e566b23
Prakash-1619 <poojariprakash88@gmail.com>
2025-10-04 16:25:50 +0530

    aadding og data

--- trial.py
+++ trial.py
@@ -1334,7 +1334,7 @@ elif page == "V2.1":
     
     train_file_path1 = "df_trained_dataset_6000.csv"  # Replace with your CSV path
     test_file_path = "test_data_20 areas_1.csv"  # Replace with your CSV path
-    train_file_path = "over_all_dataset.csv"
+    train_file_path = "over_all_dataset_og.csv"
     # --- Load Train Data ---
     try:
         df_train = pd.read_csv(train_file_path)
@@ -1621,8 +1621,8 @@ elif page == "V2.1":
                 
                 # Dataset selection in sidebar
                 st.sidebar.header("Dataset Selection")
-                dataset_options = {
-                    "Actual_data": "over_all_dataset.csv",
+                dataset_options = {"Actual_data": "over_all_dataset_og.csv"
+                    "Data without_outliers": "over_all_dataset.csv",
                     "areas_with_6000": "df_trained_dataset_6000.csv"  # Replace with your second dataset path
                 }
                 
